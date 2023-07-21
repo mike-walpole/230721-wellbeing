742e9a68429efdcb30da684dc24f76637d9833fc
mike-walpole <walpole.mike@gmail.com>
2023-07-21 14:43:17 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<div class="bg-black text-white flex flex-col items-center justify-between h-screen">
+	<div class="pt-10">
+		<p class="font-bold uppercase tracking-wider">
+			Wellbeing<span class="font-normal">Index</span>
+		</p>
+	</div>
+	<div class="flex flex-col items-center justify-betwee">
+		<div class="">01/</div>
+		<div class="">it's a present for you, Jo</div>
+	</div>
+	<div class="pb-10">
+		<img
+			src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABT0lEQVR4nO3ZPUscURgF4JssMYqIKClsgo0QMIVgY5FCUvjDTJkqnV3AzsZCLFJYqQSj6A8IpAiIiAgSEoLlEwbvwpLgR5R134H3gYVhZvfuOZxySkl3g33slbZTlbaTRYLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRaLJRR4LdnGIqYe+emvOqGftlAG95Gx8xcv7Fml+W89ofOlb4BsCTDR/XAN8x8z/FsE0vtWvHOFF34NfE2Qcn2uQU7y+axG8wnF9fIDJMkgYxVYNdIa524pgFif10TbGSgR4jo0a7AIL1xXBPM7r7U8YKZFgCOs14C+8/bsI3uBHvbWJ4RIROlitQX9jqVsEi/hZr9fwrETmqszHGviyp0j3unnWKW2AJ/jgXyt4WtoG73pKvG8KlrbCcvMZdI7SFn8AtWPBxt+WGKYAAAAASUVORK5CYII="
+			alt="arrow"
+		/>
+	</div>
+</div>
+<div class="bg-black text-white flex flex-col items-center justify-between h-screen">
+	<div />
+	<div class="flex flex-col items-center justify-betwee">
+		<div class="">02/</div>
+		<div class="">boom... let's make a business out it!</div>
+	</div>
+	<div />
+</div>
